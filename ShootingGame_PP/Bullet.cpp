e957e780ec7ff4cbc40b8aead3ff2b9ed73da797
CULRRY <rkaghwns8@naver.com>
2023-08-24 23:34:51 +0900

    [Feat] 기본 구조 설계 및 총알 함수 설계

---
+++ ShootingGame_PP/Bullet.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include "Bullet.h"
+
+
+void InitBulletList()
+{
+	gBulletListHead = (Bullet*)malloc(sizeof(Bullet));
+	gBulletListTail = (Bullet*)malloc(sizeof(Bullet));
+
+	gBulletListHead->prev = nullptr;
+	gBulletListHead->next = gBulletListTail;
+
+	gBulletListTail->prev = gBulletListHead;
+	gBulletListTail->next = nullptr;
+
+}
+
+void UpdateBullet()
+{
+	Bullet* curNode = gBulletListHead->next;
+
+	while (curNode != gBulletListTail)
+	{
+		// TODO 총알의 위치가 맵 밖일 경우 해당 총알 정보 삭제
+		if (curNode->x <= 0 || curNode->x > 78)
+		{
+			Bullet* tmp = curNode->next;
+			DeleteBulletNode(curNode);
+			curNode = tmp;
+			continue;
+		}
+
+		//TODO 업데이트 로직 구현
+
+
+		curNode = curNode->next;
+	}
+}
+
+void DeleteBulletNode(Bullet* node)
+{
+	Bullet* prevNode = node->prev;
+	Bullet* nextNode = node->next;
+
+	prevNode->next = nextNode;
+	nextNode->prev = prevNode;
+
+	free(node);
+}
+
+void PushBackBulletNode(const BulletType type, const int32 y, const int32 x)
+{
+	Bullet* addNode = (Bullet*)malloc(sizeof(Bullet));
+	addNode->type = type;
+	addNode->y = y;
+	addNode->x = x;
+
+	Bullet* lastNode = gBulletListTail->prev;
+
+	addNode->prev = lastNode;
+	addNode->next = gBulletListTail;
+	gBulletListTail->prev = addNode;
+	lastNode->next = addNode;
+}
---
+++ ShootingGame_PP/Bullet.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/// @brief 총알의 타입을 구분하는 열거형
+enum class BulletType
+{
+	PLAYER,
+	ENEMY,
+};
+
+struct Bullet
+{
+	BulletType	type;
+
+	int32		y;
+	int32		x;
+
+	Bullet*		prev;
+	Bullet*		next;
+};
+
+extern Bullet* gBulletListHead;
+extern Bullet* gBulletListTail;
+
+void InitBulletList();
+void UpdateBullet();
+void DeleteBulletNode(Bullet* node);
+void PushBackBulletNode(const BulletType type, const int32 y, const int32 x);
